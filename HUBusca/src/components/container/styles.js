a3de320067412ccaa4ae88baf8c4ed52913ba35e
Joao Michael <joaomichaelfd@gmail.com>
2022-05-30 19:36:09 -0300

    Inicialização do projeto.

---
+++ HUBusca/src/components/container/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+    width: 100%;
+    height: 100vh;
+    background: #4F388A;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`
---
+++ HUBusca/src/components/userContainer/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Section = styled.section`
+    width: 400px;
+    height: 300px;
+    background: rgba(255,255,255,.95);
+    border-radius: 10px;
+    border-bottom: 10px solid #33ADAC;
+    margin-top: 15px;
+`
