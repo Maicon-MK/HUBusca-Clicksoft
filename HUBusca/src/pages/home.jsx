684c1f43110fef9c39b1e32dbc0fa7cf0f70ad6b
Joao Michael <joaomichaelfd@gmail.com>
2022-05-30 21:23:33 -0300

    Foto de perfil do git finalizada / Iniciando a descrilçao.

--- HUBusca/src/pages/home.jsx
+++ HUBusca/src/pages/home.jsx
@@ -1,13 +1,14 @@
 import Container from '../components/container';
 import Header from '../components/header';
 import UserContainer from '../components/userContainer';
+import UserPicture from '../components/userPicture';
 
 export default function Home() {
     return (
         <Container>
             <Header />
             <UserContainer>
-                
+                <UserPicture url='https://avatars.githubusercontent.com/u/88468371?v=4' alternativText="image" />
             </UserContainer>
         </Container>
     );
