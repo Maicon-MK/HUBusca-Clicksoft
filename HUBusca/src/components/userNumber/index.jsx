371a6963241edb27345ceeeca959acaafa4cc440
Joao Michael <joaomichaelfd@gmail.com>
2022-05-30 22:55:41 -0300

    Estilização dos componentes.

---
+++ HUBusca/src/components/userNumber/index.jsx
@@ -0,0 +1,30 @@
+
+
+
+import {
+    Container,
+    NumberContainer
+
+} from './styles';
+
+
+const UserNumbers = props => (
+    <Container>
+        <NumberContainer>
+            <h1>14</h1>
+            <h2>Repositórios</h2>
+        </NumberContainer>
+
+        <NumberContainer>
+            <h1>22</h1>
+            <h2>Seguidores</h2>
+        </NumberContainer>
+
+        <NumberContainer>
+            <h1>33</h1>
+            <h2>Seguindo</h2>
+        </NumberContainer>
+    </Container>
+);
+
+export default UserNumbers;
