fa57f30eb23e45db39a9ed5d720c4de1f78229fc
Joao Michael <joaomichaelfd@gmail.com>
2022-06-01 01:24:08 -0300

    Alteração no server e packge.json2

--- HUBusca/server.js
+++ HUBusca/server.js
@@ -1,22 +1,24 @@
+const exp = require('constants');
 const express = require('express');
-
+const { resolve } = requeri('path')
+const path = require('path')
 const app = express();
-const { resolve } = require('path')
 
-app.use('/', express.static(
-
-    resolve(
-        __dirname,
-        './build'
+app.use('/',
+    express.static(
+        resolve(
+            __dirname,
+            './build'
+        )
     )
-
-  )
 )
+ app.get('/*', (req, res) =>{
+     res.sendfile(path.join(__dirname, 'build','index.html'));
+ })
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 
-app.listen(process.env.PORT || 3000, (err) => {
-    if (err) {
-        return console.log('Falhou')
-    }
-    console.log('Tudo okay')
-})
+app.listen(process.env.PORT || 3000, (err) =>{
+  err ? console.log("err" , err) : console.log("Tudo okay")
+});
