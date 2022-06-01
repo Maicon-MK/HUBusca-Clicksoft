98dc129d041c4fbcd9f6f15f12b46cba673881f3
Joao Michael <joaomichaelfd@gmail.com>
2022-06-01 00:17:39 -0300

    config express server

--- HUBusca/server.js
+++ HUBusca/server.js
@@ -1,1 +1,22 @@
-console.log('batata')
+const express = require('express');
+
+const app = express();
+const { resolve } = require('path')
+
+app.use('/', express.static(
+
+    resolve(
+        __dirname,
+        './build'
+    )
+
+  )
+)
+
+
+app.listen(process.env.PORT || 3000, (err) => {
+    if (err) {
+        return console.log('Falhou')
+    }
+    console.log('Tudo okay')
+})
